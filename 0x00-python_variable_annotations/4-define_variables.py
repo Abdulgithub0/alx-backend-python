cb329e7e233be52fa5d8822ae5c5f39821873630
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-08 12:14:23 +0100

    defined 4 type-annotated variables with values

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""define an annotated variables with their respective values"""
+
+a: int = 1
+pi: float =  3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
