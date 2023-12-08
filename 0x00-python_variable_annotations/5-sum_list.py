244e8275818881461bb9daef4c281d6c0226d18a
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-08 12:26:04 +0100

    sum list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""implement sum_list function"""
+
+
+def sum_list(input_float: list[float]) -> float:
+    """add all the ele of the list then return result"""
+    return sum(input_float)
