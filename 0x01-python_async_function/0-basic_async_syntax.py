1e0033ca4c1ab226ce2335b8d0b9b5e3e5191700
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-11 15:38:51 +0100

    randomizely delay function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-"""working with Asyncio module"""
-
 import asyncio
 import random
+
+"""working with Asyncio module"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """wait for random sec then return the sec"""
-    delay = random.uniform(0, max_delay)
+    delay = random(0, max_delay)
     await asyncio.sleep(delay)
     return delay
