f2138e5f94a83bafce100e09f84b589a95e7ddbc
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-11 15:47:36 +0100

    randomizely delay function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 
 
-import asyncio
-from random import uniform
+from asyncio import sleep
+from random import import uniform
 
 
-''' Write an asynchronous coroutine that takes in an integer
-    argument (max_delay, with a default value of 10) named
-    wait_random that waits for a random delay between 0 and
-    max_delay (included and float value) seconds and eventually returns it.
-'''
+"""working with Asyncio module
+"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Generates a random number and returns it after n delay'''
+    """wait for random sec then return the sec"""
     delay = uniform(0, max_delay)
-    await asyncio.sleep(delay)
+    await sleep(delay)
     return delay
-
-
-if __name__ == '__main__':
-    print(asyncio.run(wait_random()))
-    print(asyncio.run(wait_random(5)))
-    print(asyncio.run(wait_random(15)))
