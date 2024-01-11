5c1dcdbc8d9b0415a4e720f8794d51c3300a1861
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-11 10:13:36 +0100

    mock tested public_repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,17 @@ class TestGithubOrgClient(unittest.TestCase):
                    PropertyMock(return_value=test_org_result)):
             repos = GithubOrgClient(test_org_name)._public_repos_url
             self.assertEqual(repos, test_org_result["repos_url"])
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """mock test public_repo method"""
+        test_org_name = [{"name": "google"}, {"name": "github"}]
+        mock_get_json.return_value = test_org_name
+        with patch("client.GithubOrgClient._public_repos_url",
+                   PropertyMock(return_value="http://api.com/org/org")) as m:
+            client_instan = GithubOrgClient("google")
+            result = client_instan.public_repos()
+            match_names = [names['name'] for names in test_org_name]
+            self.assertEqual(result, match_names)
+            mock_get_json.assert_called_once_with("http://api.com/org/org")
+            m.assert_called_once()
